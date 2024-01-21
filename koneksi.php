134f00ebb183942d21a1f38a2939bb3c19e379c0
ticklelemonade <114916800+ticklelemonade@users.noreply.github.com>
2024-01-21 09:35:03 +0700

    koneksi database

--- koneksi.php
+++ koneksi.php
@@ -4,5 +4,5 @@ $koneksi = mysqli_connect("localhost", "root", "");
 	if (!$koneksi) {
 		die("Database connection problem");
 	}
-	$db_use = mysqli_select_db($koneksi, "lake_toba_db") or die("Select db problem !!");
+	$db_use = mysqli_select_db($koneksi, "db_binus_group5") or die("Select db problem !!");
 ?>
